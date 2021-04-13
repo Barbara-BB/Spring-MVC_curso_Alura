ab4ab6d607e290ce0e87f24626efb1f8827e1cb9
Barbara Bidetti <barbarabidetti@gmail.com>
2021-04-12 22:26:40 -0300

    Criação de pedido

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,11 +3,18 @@ package br.com.alura.mvc.mudi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.alura.mvc.mudi.model.Pedido;
+
 @Controller
 public class HomeController {
 	
 	@GetMapping("/home")
 	public String home() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto("xiamomi");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/41s708mKqJL._AC_.jpg");
+		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-64GB-Dual/dp/B08B3QSVM6/ref=asc_df_B08B3QSVM6/?tag=googleshopp00-20&linkCode=df0&hvadid=379773236150&hvpos=&hvnetw=g&hvrand=14329138195742527781&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001773&hvtargid=pla-976739522567&psc=1");
+		
 		return "home";
 	}
 
