794404ab1e66bc3f995e74e0c72d0f8a7b3abaf4
Barbara Bidoia Bidetti <barbarabidetti@gmail.com>
2021-04-20 23:49:13 -0300

    Inserção JPA

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -3,6 +3,10 @@ package br.com.alura.mvc.mudi.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +18,18 @@ import br.com.alura.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+
+
+	
 	@GetMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("xiamomi");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/41s708mKqJL._AC_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Celular-Xiaomi-Redmi-64GB-Dual/dp/B08B3QSVM6/ref=asc_df_B08B3QSVM6/?tag=googleshopp00-20&linkCode=df0&hvadid=379773236150&hvpos=&hvnetw=g&hvrand=14329138195742527781&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001773&hvtargid=pla-976739522567&psc=1");
-		pedido.setDescricao("decrição");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		Query query = entityManager.createQuery("select p from Pedido p",Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
+
+
 		model.addAttribute("pedidos",pedidos);
 		return "home";
 	}
